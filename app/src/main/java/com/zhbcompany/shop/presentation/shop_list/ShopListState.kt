846d939979720416c649cbff59946640449f0f28
Stefan Božanić <stefan.bozanic@computerrock.com>
2024-12-08 11:03:48 +0100

    Default sort is Completed Up

--- app/src/main/java/com/zhbcompany/shop/presentation/shop_list/ShopListState.kt
+++ app/src/main/java/com/zhbcompany/shop/presentation/shop_list/ShopListState.kt
@@ -6,7 +6,7 @@ import com.zhbcompany.shop.domain.util.ShopItemOrder
 
 data class ShopListState(
     val shopItemDomains: List<ShopItemDomain> = emptyList(),
-    val shopItemOrder: ShopItemOrder = ShopItemOrder.Store(SortingDirection.Down),
+    val shopItemOrder: ShopItemOrder = ShopItemOrder.Completed(SortingDirection.Up),
     val isLoading: Boolean = true,
     val error: String? = null
 )
--- app/src/main/java/com/zhbcompany/shop/presentation/util/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.zhbcompany.shop.presentation.util
-
-sealed class Screen(val route: String) {
-    object ShopItemListScreen: Screen("shopItemList_screen")
-    object ShopNewUpdateScreen: Screen("shopNewUpdate_screen")
-}
